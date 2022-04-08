acfda337874427179d34c0b0e48465d63a95f182
root <root@817813-ci00258.tmweb.ru>
2022-04-08 18:29:01 +0300

    backup

---
+++ README.md
@@ -0,0 +1,2 @@
+./manage.py get_data
+./manage.py parse
--- composeexample/parsing/models.py
+++ composeexample/parsing/models.py
@@ -4,7 +4,28 @@ from django.db import models
 class Network(models.Model):
     url = models.CharField(max_length=512, verbose_name='Ссылка')
     name = models.CharField(max_length=512, verbose_name='Название')
+    description  = models.CharField(max_length=512, blank=True, default="", verbose_name='Анонс')
 
     class Meta:
         verbose_name_plural = "Торговые сети"
         verbose_name = "Торговая сеть"
+    
+
+    def __str__(self):
+        # return f"{self._meta.model_name} object {self.id}"
+        # return f"ха-ха {self.name} {self.url}"
+        return self.name
+
+    
+    def __str__(self):
+        return self.name
+
+class About(models.Model):
+    verbose_description = models.CharField(max_length=512, verbose_name=" About")
+
+    class Meta:
+        verbose_name = "О нас"
+        verbose_name_plural = "О нас"
+
+
+    
